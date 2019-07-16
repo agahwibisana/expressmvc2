4d42f148b351b476a539001a7ebfb8927486c20c
Agah <fajaranugrah@outlook.com>
2019-07-16 21:48:19 +0700

    '5th'

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-var express = require('express');
-var app = express();
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-//initialize server
-// app.listen(1337, function(){
-//     console.log('server is ready');
-// });
-
-app.get('/',function(req,res){
-    res.send('hello world');
-})
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.send('hello'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
