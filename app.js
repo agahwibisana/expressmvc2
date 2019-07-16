4a18cfc34c6cf7951b1d56d54cf2044960370fc9
Agah <fajaranugrah@outlook.com>
2019-07-16 21:44:38 +0700

    '4th'

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ var express = require('express');
 var app = express();
 
 //initialize server
-app.listen(1337, function(){
-    console.log('server is ready');
-});
+// app.listen(1337, function(){
+//     console.log('server is ready');
+// });
 
 app.get('/',function(req,res){
     res.send('hello world');
